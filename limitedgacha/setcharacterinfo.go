c432120c363a82e386cfcb5abd5b21663470acfe
Rabishop <50069587+Rabishop@users.noreply.github.com>
2022-12-16 14:31:15 +0900

    Add new character

---
+++ limitedgacha/setcharacterinfo.go
@@ -0,0 +1,38 @@
+package limitedgacha
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+type Characterinfo struct {
+	Characterid uint `gorm:"primaryKey"`
+	Name        string
+	Stdpower    uint
+}
+
+// read db
+func ConnSetInfo() {
+	// try to connect db
+	dsn := rUsername + ":" + rPassword + "@" + rProtocol + "(" + rAddress + ")" + "/" + rDbname
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{SingularTable: true}})
+	if err != nil {
+		panic(err)
+	}
+
+	var characterinfo = []Characterinfo{
+		{Characterid: 11, Name: "Skadi the Corrupting Heart", Stdpower: 3000},
+	}
+
+	//insert new character information
+	SQLrequest := db.Create(characterinfo)
+	err = SQLrequest.Error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
